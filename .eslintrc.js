6b599f380bb52721e793053846bb80dcfc8e677b
Kelly Anne Pipe <kellyanne.pipe@gmail.com>
2021-12-01 21:01:24 -0500

    Ugh lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,15 @@
 module.exports = {
-  'env': {
-    'es6': true,
-    'node': true,
-    'mocha': true
+  env: {
+    es6: true,
+    node: true,
+    mocha: true,
   },
-  'extends': 'eslint:recommended',
-  'parserOptions': {
-    'sourceType': 'module'
+  extends: "eslint:recommended",
+  parserOptions: {
+    sourceType: "module",
   },
-  'rules': {
-    'indent': ['error', 2, { 'SwitchCase': 1 }],
-    'quotes': [2, 'single'],
-    'semi': 2
-  }
+  rules: {
+    indent: ["error", 2, { SwitchCase: 1 }],
+    semi: 2,
+  },
 };
